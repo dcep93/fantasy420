d8d14a2a8d5e2ee5a06022b719431569341715c2
dcep93 <dcep93@gmail.com>
2022-08-21 22:23:41 -0700

    x

---
+++ app/fantasy420/src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+import fantasy420 from "./fantasy420";
+
+function App() {
+  return fantasy420();
+}
+
+export default App;
---
+++ app/fantasy420/src/fantasy420/index.tsx
@@ -0,0 +1,7 @@
+import Fantasy420 from "./pages/ff";
+
+function index() {
+  return <Fantasy420 />;
+}
+
+export default index;
