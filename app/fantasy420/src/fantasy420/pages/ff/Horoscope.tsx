30f8b4159ffe4425ba3772224fc8feab564982a3
dcep93 <dcep93@gmail.com>
2022-09-02 21:05:56 -0400

    x

--- app/fantasy420/src/fantasy420/pages/ff/Horoscope.tsx
+++ app/fantasy420/src/fantasy420/pages/ff/Horoscope.tsx
@@ -3,19 +3,44 @@ import FlexColumns from "../../FlexColumns";
 import horoscope from "./horoscope.json";
 
 function Horoscope() {
+  const results = get_results(horoscope);
   return (
     <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
       <FlexColumns
-        columns={Object.entries(horoscope).map(([key, arr]) => (
+        columns={Object.entries(results).map(([key, arr]) => (
           <pre key={key}>
             {key}
             <br />
-            {JSON.stringify(arr, null, 2)}
+            {JSON.stringify(
+              arr.map(
+                (o) =>
+                  `${o.name} (${o.sign}) + ${o.qbname} (${o.qbsign}) = ${o.direct_compatibility}`
+              ),
+              null,
+              2
+            )}
           </pre>
         ))}
       />
     </div>
   );
+}
+
+function get_results(horoscope: any[][]) {
+  const h = horoscope
+    .map(([name, sign, qbname, qbsign, direct_compatibility, adp]) => ({
+      name,
+      sign,
+      qbname,
+      qbsign,
+      direct_compatibility,
+      adp,
+    }))
+    .sort((a, b) => a.adp - b.adp);
+  return {
+    good: h.filter((o) => o.direct_compatibility > 0),
+    bad: h.filter((o) => o.direct_compatibility < 0),
+  };
 }
 
 export default Horoscope;
