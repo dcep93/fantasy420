f8714991b862bac83ffabbf59bc077f7943c444a
dcep93 <dcep93@gmail.com>
2022-09-08 14:07:02 -0400

    x
    x
    
    x
    
    x
    
    x

---
+++ extension/content_script.js
@@ -0,0 +1,50 @@
+async function execute() {
+  const paths = [
+    {
+      p: /https:\/\/fantasy\.espn\.com\/football\/draft.*/,
+      jss: [
+        "firebase/firebase-app.js",
+        "firebase/firebase-database.js",
+        "draft.js",
+      ],
+    },
+    {
+      p: /https:\/\/fantasy\.espn\.com\/football\/mockdraftlobby/,
+      jss: ["mockdraftlobby.js"],
+    },
+    {
+      p: /https:\/\/fantasy\.espn\.com\/football.*/,
+      jss: ["reddit/shared.js", "reddit/inject.js"],
+    },
+  ];
+
+  const jss = paths
+    .filter((o) => location.href.match(o.p))
+    .flatMap((o) => o.jss);
+
+  for (let i = 0; i < jss.length; i++) {
+    await fileToPromise(jss[i]);
+  }
+}
+
+function fileToPromise(fileName) {
+  const url = chrome.runtime.getURL(`extensions/${fileName}`);
+  if (fileName.endsWith(".txt")) {
+    const d = document.createElement("data");
+    d.setAttribute("id", fileName);
+    document.head.appendChild(d);
+    return fetch(url)
+      .then((resp) => resp.text())
+      .then((text) => (d.innerHTML = text));
+  }
+  const s = document.createElement("script");
+  s.src = url;
+  s.setAttribute("storage", chrome.storage);
+
+  return new Promise((resolve, reject) => {
+    s.onload = resolve;
+    document.head.appendChild(s);
+  });
+}
+
+execute();
---
+++ extension/extensions/vegas.js
@@ -0,0 +1,26 @@
+// https://sportsbook.draftkings.com/leagues/football/nfl?category=rush/rec-props&subcategory=receptions
+
+// fetch(
+//   "https://sportsbook-us-ny.draftkings.com//sites/US-NY-SB/api/v5/eventgroups/88808/categories/1001/subcategories/9512?format=json",
+//   {
+//     headers: {
+//       accept: "*/*",
+//       "accept-language": "en-US,en;q=0.9",
+//       "cache-control": "no-cache",
+//       pragma: "no-cache",
+//       "sec-ch-ua":
+//         '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+//       "sec-ch-ua-mobile": "?0",
+//       "sec-ch-ua-platform": '"macOS"',
+//       "sec-fetch-dest": "empty",
+//       "sec-fetch-mode": "cors",
+//       "sec-fetch-site": "same-site",
+//     },
+//     referrer: "https://sportsbook.draftkings.com/",
+//     referrerPolicy: "strict-origin-when-cross-origin",
+//     body: null,
+//     method: "GET",
+//     mode: "cors",
+//     credentials: "include",
+//   }
+// );
