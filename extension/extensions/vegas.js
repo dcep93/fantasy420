f7249fc2844e1f723f8a4a242152ffb259146254
dcep93 <dcep93@gmail.com>
2022-09-09 11:31:44 -0400

    x

--- extension/extensions/vegas.js
+++ extension/extensions/vegas.js
@@ -1,10 +1,25 @@
 (() => {
+  const extension_id = "codiminongikfnidmdkpmeigapidedgn";
+
   const INTERVAL_MS = 1000;
+  const MAX_FUTURE_GAME_MS = 7 * 24 * 60 * 60 * 1000;
 
   var cache = {};
 
+  var data = undefined;
+
   function main() {
-    return vegas().then(() => setTimeout(main, INTERVAL_MS));
+    return new Promise((resolve, reject) =>
+      chrome.runtime
+        ? vegas()
+            .then((_data) => (data = _data))
+            .then(resolve)
+        : data !== undefined
+        ? resolve(data)
+        : reject("chrome.runtime not defined")
+    )
+      .then(inject)
+      .then(() => setTimeout(main, INTERVAL_MS));
   }
 
   function vegas() {
@@ -13,7 +28,12 @@
       12 * 60 * 60 * 1000
     )
       .then(({ eventGroup }) => eventGroup)
-      .then(({ events }) => events.filter(({ startDate }) => startDate))
+      .then(({ events }) =>
+        events.filter(
+          ({ startDate }) =>
+            new Date(startDate) - Date.now() < MAX_FUTURE_GAME_MS
+        )
+      )
       .then((events) =>
         events.map(({ eventId }) =>
           fetchC(
@@ -45,25 +65,27 @@
             )
           )
         )
+      );
+  }
+
+  function inject(events) {
+    console.log(events);
+    return Promise.resolve()
+      .then(() => document.getElementsByClassName("player-column__bio"))
+      .then(Array.from)
+      .then((arr) =>
+        arr
+          .map((bio) => bio.getElementsByTagName("a")[0])
+          .filter(Boolean)
+          .map((a) => ({ a, name: a.innerText }))
       )
-      .then((events) =>
-        Promise.resolve()
-          .then(() => document.getElementsByClassName("player-column__bio"))
-          .then(Array.from)
-          .then((arr) =>
-            arr
-              .map((bio) => bio.getElementsByTagName("a")[0])
-              .filter(Boolean)
-              .map((a) => ({ a, name: a.innerText }))
-          )
-          .then((arr) =>
-            arr.map(({ a, name }) =>
-              getOdds(name, events).then((json) => ({ a, name, json }))
-            )
-          )
-          .then((arr) =>
-            arr.map(({ a, json }) => (a.title = JSON.stringify(json, null, 2)))
-          )
+      .then((arr) =>
+        arr.map(({ a, name }) =>
+          getOdds(name, events).then((json) => ({ a, name, json }))
+        )
+      )
+      .then((arr) =>
+        arr.map(({ a, json }) => (a.title = JSON.stringify(json, null, 2)))
       );
   }
 
@@ -75,12 +97,18 @@
     const cached = cache[url];
     const now = Date.now();
     if (now - cached?.timestamp < maxAgeMs) return Promise.resolve(cached.json);
-    return fetch(url)
-      .then((resp) => resp.json())
-      .then((json) => {
-        cache[url] = { timestamp: now, json };
-        return json;
-      });
+    return new Promise((resolve) =>
+      chrome.runtime.sendMessage(
+        extension_id,
+        { fetch: { url, json: true } },
+        function (response) {
+          resolve(response);
+        }
+      )
+    ).then((json) => {
+      cache[url] = { timestamp: now, json };
+      return json;
+    });
   }
 
   main();
