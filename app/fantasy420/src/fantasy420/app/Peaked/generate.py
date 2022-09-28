42b0c1f328472e9dbbdacdfc9461e6b3685af283
dcep93 <dcep93@gmail.com>
2022-09-28 15:24:34 -0400

    x

---
+++ app/fantasy420/src/fantasy420/app/Peaked/generate.py
@@ -0,0 +1,9 @@
+url = "https://i0.wp.com/peakedinhighskool.com/wp-content/uploads/2022/09/1QB1.0PPR4pt_20220927.png?w=1790&ssl=1"
+
+
+def main():
+    print(url)
+
+
+if __name__ == "__main__":
+    main()
