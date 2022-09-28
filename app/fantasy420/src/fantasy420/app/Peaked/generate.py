e820a3513b659fdb53381966740089fd421f9ef9
dcep93 <dcep93@gmail.com>
2022-09-28 16:11:45 -0400

    image

--- app/fantasy420/src/fantasy420/app/Peaked/generate.py
+++ app/fantasy420/src/fantasy420/app/Peaked/generate.py
@@ -1,8 +1,16 @@
+import requests
+
+import cv2
+import numpy
+import pytesseract
+
 url = "https://i0.wp.com/peakedinhighskool.com/wp-content/uploads/2022/09/1QB1.0PPR4pt_20220927.png?w=1790&ssl=1"
 
 
 def main():
-    print(url)
+    raw = requests.get(url).content
+    data = numpy.frombuffer(raw, dtype='uint8')
+    image = cv2.imdecode(data, cv2.IMREAD_GRAYSCALE)
 
 
 if __name__ == "__main__":
