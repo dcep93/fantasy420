a83af6fa82288aceaaa74245d52787d81411b9f5
dcep93 <dcep93@gmail.com>
2022-09-28 16:56:02 -0400

    x

--- app/fantasy420/src/fantasy420/app/Peaked/index.tsx
+++ app/fantasy420/src/fantasy420/app/Peaked/index.tsx
@@ -1,46 +1,10 @@
-import React from "react";
+import raw_generated from "./generated.json";
 
-const url =
-  "https://pbs.twimg.com/media/FcnWjXvWYAAiUTm?format=jpg&name=4096x4096";
-
-type ValuesType = { [playerName: string]: number };
+const generated: {
+  peaked: { url: string; text: string };
+  teams: { name: string; players: string[] }[];
+} = raw_generated;
 
 export default function Peaked() {
-  return <SubPeaked />;
-}
-
-function parse(data: string): ValuesType {
-  return { sup: 123, dude: 456 };
-}
-
-class SubPeaked extends React.Component<{}, { values: ValuesType }> {
-  componentDidMount(): void {
-    (window.location.href.startsWith("http://localhost:")
-      ? fetch(url)
-      : fetch("https://proxy420.appspot.com/", {
-          method: "POST",
-          body: JSON.stringify({ url, maxAgeMs: 10 * 60 * 1000 }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-    )
-      .then((resp) => resp.text())
-      .then(parse)
-      .then((values) => this.setState({ values }));
-  }
-
-  render() {
-    return (
-      <div>
-        {Object.entries(this.state?.values || {}).map(([playerName, value]) => (
-          <div key={playerName}>
-            <span>{playerName}</span>
-            <span> </span>
-            <span>{value}</span>
-          </div>
-        ))}
-      </div>
-    );
-  }
+  return <pre>{JSON.stringify(generated, null, 2)}</pre>;
 }
