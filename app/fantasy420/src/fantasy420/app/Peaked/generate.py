b2a470a1b7ae410f1dfaf6b6bd5324c1c3eae342
dcep93 <dcep93@gmail.com>
2022-10-26 11:08:43 -0400

    x

--- app/fantasy420/src/fantasy420/app/Peaked/generate.py
+++ app/fantasy420/src/fantasy420/app/Peaked/generate.py
@@ -17,8 +17,9 @@ league_id = 203836968
 # https://i0.wp.com/peakedinhighskool.com/wp-content/uploads/2022/09/1QB1.0PPR4pt_20220927.png?w=1790&ssl=1
 # https://pbs.twimg.com/media/FeTjnmBXEAI5EM6?format=jpg&name=4096x4096
 # https://pbs.twimg.com/media/Fe3mDJPXkAIn2Sj?format=jpg&name=4096x4096
+# https://pbs.twimg.com/media/FfbqE5qX0AA-fyV?format=jpg&name=4096x4096
 peaked_url = '''
-https://pbs.twimg.com/media/FfbqE5qX0AA-fyV?format=jpg&name=4096x4096
+https://pbs.twimg.com/media/Ff_tdS8XoAAaJom?format=jpg&name=4096x4096
 '''.strip()
 teams_url = f"https://fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{league_id}?view=mRoster&view=mTeam"
 
