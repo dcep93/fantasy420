3e2f5f6e2631f11e19d9767339af65f878e770f2
dcep93 <dcep93@gmail.com>
2023-08-21 22:56:53 -0400

    x

--- app/fantasy420/src/fantasy420/app/Accuracy/accuracy.py
+++ app/fantasy420/src/fantasy420/app/Accuracy/accuracy.py
@@ -2,6 +2,11 @@ import json
 
 
 def getPlayers():
+    # name = player["player"]["fullName"]
+    # stat = [
+    #     stat for stat in player["player"]["stats"]
+    #     if stat["id"] == "002022"
+    # ][0]
     return []
 
 
@@ -9,25 +14,17 @@ def main():
     with open("./accuracy_input.json") as fh:
         accuracy_input = json.load(fh)
 
-    espn = {}
-    missing = []
-    for player in getPlayers():
-        name = player["player"]["fullName"]
-        stat = [
-            stat for stat in player["player"]["stats"]
-            if stat["id"] == "002022"
-        ][0]
-        espn[name] = {
+    espn = {
+        name: {
             "position": accuracy_input["players"][name]["position"],
             "adp": accuracy_input["adp"][name],
             "auction": -accuracy_input["avc"][name],
             "season_points": stat["appliedTotal"],
             "average_points": stat["appliedAverage"],
         }
+        for name, stat in getPlayers()
+    }
 
-    if missing:
-        print(missing)
-        return
     print(json.dumps({"espn": espn, "sources": accuracy_input["extra"]}))
 
 
