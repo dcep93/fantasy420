c929f8509069e9b9621840c83c6a0588a3c618e0
dcep93 <dcep93@gmail.com>
2023-09-12 12:21:23 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
@@ -8,11 +8,22 @@ export const wrapped: WrappedType = rawWrapped;
 export default function Wrapped() {
   document.title = "Fantasy Wrapped";
   const toRender: { [key: string]: () => ReactNode } = {
+    SqueezesAndStomps,
+    WeekTopsAndBottoms,
+    BestByPosition,
+    DeterminedByDiscreteScoring,
+    ChosenWrong,
+    GooseEggs,
+    UniquesStarted,
+    BoomBust,
     json,
   };
-  const [toRenderKey, update] = useState(
-    window.location.hash.substring(1) || Object.keys(toRender)[0]!
-  );
+  var hashKey = window.location.hash.substring(1);
+  if (!toRender[hashKey]) {
+    window.location.hash = "";
+    hashKey = "";
+  }
+  const [toRenderKey, update] = useState(hashKey || Object.keys(toRender)[0]!);
   return (
     <div style={{ fontFamily: "'Courier New', Courier, monospace" }}>
       <div
@@ -21,14 +32,7 @@ export default function Wrapped() {
         {Object.keys(toRender).map((key, i) => (
           <div
             key={i}
-            style={{
-              backgroundColor: "white",
-              display: "inline-block",
-              borderRadius: "1em",
-              border: "2px solid black",
-              padding: "0.7em",
-              margin: "0.5em",
-            }}
+            style={bubbleStyle}
             onClick={() => {
               window.location.hash = key;
               update(key);
@@ -39,23 +43,40 @@ export default function Wrapped() {
         ))}
       </div>
       <div>
-        <h1
-          style={{
-            backgroundColor: "white",
-            display: "inline-block",
-            borderRadius: "1em",
-            border: "2px solid black",
-            padding: "0.7em",
-            margin: "0.5em",
-          }}
-        >
-          {toRenderKey}
-        </h1>
+        <h1 style={bubbleStyle}>{toRenderKey}</h1>
         <div>{toRender[toRenderKey]!()}</div>
       </div>
     </div>
   );
 }
+
+enum Position {
+  QB = 1,
+  RB = 2,
+  WR = 3,
+  TE = 4,
+  K = 5,
+  DST = 16,
+}
+
+function count(arr: string[]): { [key: string]: number } {
+  const c: { [key: string]: number } = {};
+  arr.forEach((k) => {
+    c[k] = (c[k] || 0) + 1;
+  });
+  return c;
+}
+
+const bubbleStyle = {
+  backgroundColor: "white",
+  display: "inline-block",
+  borderRadius: "1em",
+  border: "2px solid black",
+  padding: "0.7em",
+  margin: "0.5em",
+};
+
+//
 
 function json() {
   return (
@@ -65,11 +86,75 @@ function json() {
   );
 }
 
-// WeekTopsAndBottoms: WeekWinnersAndLosers(data),
-// SqueezesAndStomps: SqueezesAndStomps(data),
-// BestByPosition: BestByStreamingPosition(data),
-// DeterminedByDiscreteScoring: GamesDeterminedByDiscreteScoring(data),
-// ChosenWrong: TimesChosenWrong(data),
-// GooseEggs: GooseEggs(data),
-// StudsStarted: StudsStarted(data),
-// BoomBust: BoomBust(data),
+function SqueezesAndStomps() {
+  return <div></div>;
+}
+
+function WeekTopsAndBottoms() {
+  return <div></div>;
+}
+
+function BestByPosition() {
+  return <div></div>;
+}
+
+function DeterminedByDiscreteScoring() {
+  return <div></div>;
+}
+
+function ChosenWrong() {
+  return <div></div>;
+}
+
+function GooseEggs() {
+  return <div></div>;
+}
+
+function UniquesStarted() {
+  return (
+    <div>
+      {Object.values(Position)
+        .filter((p) => Number.isInteger(p))
+        .map((p) => p as Position)
+        .map((p) => (
+          <div key={p}>
+            <div style={bubbleStyle}>
+              {Position[p]}
+              <div>
+                {Object.values(wrapped.ffTeams)
+                  .map((ffTeam) => ({
+                    teamName: ffTeam.name,
+                    started: Object.entries(
+                      count(
+                        Object.values(ffTeam.rosters).flatMap(
+                          (roster) => roster.starting
+                        )
+                      )
+                    )
+                      .map(([playerId, c]) => ({
+                        player: wrapped.nflPlayers[playerId],
+                        c,
+                      }))
+                      .filter(({ player }) => player?.position === Position[p])
+                      .map(({ player, c }) => `${player.name}: ${c}`),
+                  }))
+                  .sort((a, b) => a.started.length - b.started.length)
+                  .map(({ teamName, started }, i) => (
+                    <div key={i}>
+                      <b>
+                        {teamName}: ({started.length})
+                      </b>{" "}
+                      - {started.join(" , ")}
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+function BoomBust() {
+  return <div></div>;
+}
