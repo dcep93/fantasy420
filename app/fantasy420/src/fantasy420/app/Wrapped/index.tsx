bb763e1f33e0362a2ff9e64a66b60287b95e20e6
dcep93 <dcep93@gmail.com>
2023-09-18 10:39:12 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
@@ -19,6 +19,7 @@ export default function Wrapped() {
       BoomBust,
       Accuracy,
       OwnedTeams,
+      Benchwarmers,
       json,
     }).map(([k, v]) => [k, v()])
   );
@@ -633,6 +634,66 @@ function BoomBust() {
             </table>
           </div>
         ))}
+      </div>
+    </div>
+  );
+}
+
+function Benchwarmers() {
+  const weeks = Object.keys(Object.values(wrapped.ffTeams)[0].rosters).filter(
+    (weekNum) => weekNum !== "0"
+  );
+  return (
+    <div>
+      <div style={bubbleStyle}>
+        {weeks
+          .flatMap((weekNum) =>
+            Object.values(wrapped.ffTeams).map((team) => ({
+              teamName: team.name,
+              weekNum,
+              score: team.rosters[weekNum].rostered
+                .filter(
+                  (playerId) =>
+                    !team.rosters[weekNum].starting.includes(playerId)
+                )
+                .map(
+                  (playerId) =>
+                    wrapped.nflPlayers[playerId].scores[weekNum] || 0
+                )
+                .reduce((a, b) => a + b, 0),
+            }))
+          )
+          .sort((a, b) => b.score - a.score)
+          .map((o, i) => (
+            <div key={i}>
+              [{o.teamName}] bench week {o.weekNum} scored {toFixed(o.score)}
+            </div>
+          ))}
+      </div>
+      <div style={bubbleStyle}>
+        {weeks
+          .flatMap((weekNum) =>
+            Object.values(wrapped.ffTeams).flatMap((team) =>
+              team.rosters[weekNum].rostered
+                .filter(
+                  (playerId) =>
+                    !team.rosters[weekNum].starting.includes(playerId)
+                )
+                .map((playerId) => ({
+                  teamName: team.name,
+                  weekNum,
+                  player: wrapped.nflPlayers[playerId],
+                }))
+            )
+          )
+          .map((o) => ({ ...o, score: o.player.scores[o.weekNum]! }))
+          .sort((a, b) => b.score - a.score)
+          .slice(0, weeks.length * Object.keys(wrapped.ffTeams).length)
+          .map((o, i) => (
+            <div key={i}>
+              [{o.teamName}]: [{o.player.name}] week {o.weekNum}: {o.score}
+            </div>
+          ))}
       </div>
     </div>
   );
