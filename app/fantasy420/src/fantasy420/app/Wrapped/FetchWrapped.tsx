2d7bc8f23ed15adfb1dcc426224b578021dbf881
dcep93 <dcep93@gmail.com>
2023-09-27 18:30:17 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/FetchWrapped.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/FetchWrapped.tsx
@@ -473,8 +473,8 @@ export default function FetchWrapped() {
                                   : {
                                       key: teamId.toString(),
                                       value: {
-                                        yardsAllowed: stats["127"],
-                                        pointsAllowed: stats["187"],
+                                        yardsAllowed: stats["127"] || 0,
+                                        pointsAllowed: stats["187"] || 0,
                                       },
                                     }
                               )
