7934e475bf1d025e7b50161c460f0df768b1d36f
dcep93 <dcep93@gmail.com>
2023-10-22 09:16:41 -0400

    x

--- app/fantasy420/src/fantasy420/app/PlayoffPredictability/index.tsx
+++ app/fantasy420/src/fantasy420/app/PlayoffPredictability/index.tsx
@@ -1,3 +1,59 @@
-export default function index() {
-  return <div>gotem</div>;
+import { useState } from "react";
+import rawData from "./data.json";
+
+type DataType = {
+  [year: string]: {
+    playoff_teams: string[];
+    primetimes: { [category: string]: { [team: string]: number } };
+  };
+};
+
+const data: DataType = rawData;
+
+export default function Index() {
+  const defaultYear = Object.keys(data).sort().reverse()[0];
+  const [year, updateYear] = useState(defaultYear);
+  const yearData = data[year];
+  const predictabilities: { [category: string]: number } = Object.fromEntries(
+    Object.entries(yearData.primetimes).map(([category, counts]) => [
+      category,
+      1,
+    ])
+  );
+  return (
+    <div>
+      <div>
+        year:{" "}
+        <select
+          defaultValue={defaultYear}
+          onChange={(e) => updateYear(e.target.value)}
+        >
+          {Object.keys(data).map((y) => (
+            <option key={y} value={y}>
+              {y}
+            </option>
+          ))}
+        </select>
+      </div>
+      <table>
+        <tr>
+          <td>playoff teams</td>
+          <td>{yearData.playoff_teams.join(", ")}</td>
+        </tr>
+        <tr>
+          <td>predicted playoff teams</td>
+          <td>{yearData.playoff_teams.join(", ")}</td>
+        </tr>
+        <tr>
+          <td>predictabilities</td>
+        </tr>
+        {Object.entries(predictabilities).map(([category, predictability]) => (
+          <tr key={category}>
+            <td>{category}</td>
+            <td>{predictability}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 }
