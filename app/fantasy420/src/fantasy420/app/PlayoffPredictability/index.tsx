ac2e8225b5f0db007b101c91f650aed39a4c12d0
dcep93 <dcep93@gmail.com>
2023-10-22 09:26:16 -0400

    x

--- app/fantasy420/src/fantasy420/app/PlayoffPredictability/index.tsx
+++ app/fantasy420/src/fantasy420/app/PlayoffPredictability/index.tsx
@@ -14,12 +14,33 @@ export default function Index() {
   const defaultYear = Object.keys(data).sort().reverse()[0];
   const [year, updateYear] = useState(defaultYear);
   const yearData = data[year];
+  const allTeams = Object.keys(
+    Object.fromEntries(
+      Object.values(yearData.primetimes)
+        .map((counts) => Object.keys(counts))
+        .flatMap((team) => team)
+        .map((team) => [team, true])
+    )
+  );
   const predictabilities: { [category: string]: number } = Object.fromEntries(
     Object.entries(yearData.primetimes).map(([category, counts]) => [
       category,
       1,
     ])
   );
+  const prediction = allTeams
+    .map((team) => ({
+      team,
+      score: Object.entries(yearData.primetimes)
+        .map(
+          ([category, counts]) =>
+            predictabilities[category] * (counts[team] || 0)
+        )
+        .reduce((a, b) => a + b, 0),
+    }))
+    .sort((a, b) => a.score - b.score)
+    .map((o) => o.team)
+    .slice(-yearData.playoffs.length);
   return (
     <div>
       <div>
@@ -36,23 +57,27 @@ export default function Index() {
         </select>
       </div>
       <table>
-        <tr>
-          <td>playoff teams</td>
-          <td>{yearData.playoffs.join(", ")}</td>
-        </tr>
-        <tr>
-          <td>predicted playoff teams</td>
-          <td>{yearData.playoffs.join(", ")}</td>
-        </tr>
-        <tr>
-          <td>predictabilities</td>
-        </tr>
-        {Object.entries(predictabilities).map(([category, predictability]) => (
-          <tr key={category}>
-            <td>{category}</td>
-            <td>{predictability.toFixed(2)}</td>
+        <tbody>
+          <tr>
+            <td>playoff teams</td>
+            <td>{yearData.playoffs.join(", ")}</td>
           </tr>
-        ))}
+          <tr>
+            <td>predicted playoff teams</td>
+            <td>{prediction.join(", ")}</td>
+          </tr>
+          <tr>
+            <td>predictabilities</td>
+          </tr>
+          {Object.entries(predictabilities).map(
+            ([category, predictability]) => (
+              <tr key={category}>
+                <td>{category}</td>
+                <td>{predictability.toFixed(2)}</td>
+              </tr>
+            )
+          )}
+        </tbody>
       </table>
     </div>
   );
