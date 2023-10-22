57afba81d628ced66a26bf74f947827eae7e2ec8
dcep93 <dcep93@gmail.com>
2023-10-22 12:56:37 -0400

    x

--- app/fantasy420/src/fantasy420/app/PlayoffPredictability/index.tsx
+++ app/fantasy420/src/fantasy420/app/PlayoffPredictability/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { printF } from "../Fetch";
 import rawData from "./data.json";
 import rawPredictabilities from "./predictabilities.json";
 
 type DataType = {
   [year: string]: {
     [team: string]: {
-      playoffs: boolean;
+      playoffs: number;
       primetimes: { [category: string]: number };
     };
   };
@@ -15,12 +16,110 @@ const data: DataType = rawData;
 const allPredictabilities: { [year: string]: { [category: string]: number } } =
   rawPredictabilities;
 
+function fetchData() {
+  function clog<T>(t: T) {
+    console.error(t);
+    return t;
+  }
+  function countStrings(arr: string[]): { [key: string]: number } {
+    const c: { [key: string]: number } = {};
+    arr.forEach((k) => {
+      c[k] = (c[k] || 0) + 1;
+    });
+    return c;
+  }
+  return Promise.resolve(Array.from(new Array(10)).map((_, i) => 2022 - i))
+    .then((years) =>
+      years.map((year) =>
+        fetch(`https://www.pro-football-reference.com/years/${year}/games.htm`)
+          .then((resp) => resp.text())
+          .then((rawHtml) =>
+            new DOMParser().parseFromString(rawHtml, "text/html")
+          )
+          .then((html) => html.getElementById("games")!)
+          .then((table) => {
+            const headers = Array.from(
+              table.getElementsByTagName("thead")[0].getElementsByTagName("th")
+            ).map((th, i) => th.innerText || i.toString());
+            return Array.from(
+              table.getElementsByTagName("tbody")[0].getElementsByTagName("tr")
+            )
+              .filter((tr) => tr.className !== "thead")
+              .map((tr) =>
+                Object.fromEntries(
+                  Array.from(tr.children).map((td, i) => [
+                    headers[i],
+                    (td as HTMLElement).innerText,
+                  ])
+                )
+              );
+          })
+          .then((rows) =>
+            rows
+              .map((row) => ({
+                teams: ["Winner/tie", "Loser/tie"].map(
+                  (k) => row[k].split(" ").reverse()[0]
+                ),
+                isPlayoffs: isNaN(parseInt(row["Week"])),
+                category:
+                  row["Day"] !== "Sun"
+                    ? row["Day"]
+                    : row["Time"].startsWith("1:") ||
+                      row["Time"].startsWith("4:")
+                    ? "Sun"
+                    : `${row["Day"]} ${row["Time"].split(":")[0]}${row[
+                        "Time"
+                      ].slice(-2)}`,
+                row,
+              }))
+              .filter((row) => row.category !== "Sat")
+              .filter((row) => row.category !== "Sun")
+          )
+          .then((matches) =>
+            Object.fromEntries(
+              Object.keys(
+                Object.fromEntries(
+                  matches
+                    .flatMap((match) => match.teams)
+                    .map((team) => [team, 1])
+                )
+              )
+                .filter((team) => team !== "")
+                .map((team) => ({
+                  team,
+                  matches: matches.filter((match) =>
+                    match.teams.includes(team)
+                  ),
+                }))
+                .map(({ team, matches }) => [
+                  team,
+                  {
+                    playoffs: matches.filter((match) => match.isPlayoffs)
+                      .length,
+                    primetimes: countStrings(
+                      matches
+                        .filter((match) => !match.isPlayoffs)
+                        .map((match) => match.category)
+                    ),
+                  },
+                ])
+            )
+          )
+          .then((yearData) => [year, yearData])
+      )
+    )
+    .then((ps) => Promise.all(ps))
+    .then((dataArr) => Object.fromEntries(dataArr))
+    .then(clog);
+}
+
 export default function Index() {
+  console.log(printF(fetchData));
   const defaultYear = Object.keys(data).sort().reverse()[0];
   const [year, updateYear] = useState(defaultYear);
   const yearData = data[year];
   const predictabilities = allPredictabilities[year];
-  const playoffs = Object.entries(yearData)
+  const playoffs = Object.entries(yearData) // todo
     .map(([team, teamData]) => ({ team, teamData }))
     .filter((o) => o.teamData.playoffs)
     .map((o) => o.team);
