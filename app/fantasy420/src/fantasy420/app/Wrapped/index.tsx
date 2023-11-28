fdc64cb25ecca2d101477b58fe227bf683320eb4
dcep93 <dcep93@gmail.com>
2023-11-27 21:29:08 -0500

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
@@ -613,16 +613,21 @@ function Negatives() {
         .flatMap((p) =>
           Object.entries(p.scores).map(([weekNum, score]) => ({
             ...p,
+            started: Object.values(wrapped.ffTeams).find((t) =>
+              t.rosters[weekNum].starting.includes(p.id)
+            )?.name,
             weekNum,
             score: score!,
           }))
         )
         .filter((p) => p.weekNum !== "0")
+        .filter((p) => p.position !== Position[Position.DST])
         .filter((p) => p.score < 0)
         .sort((a, b) => a.score - b.score)
         .map((p, i) => (
           <div key={i}>
-            {p.name} scored {p.score} week {p.weekNum}
+            week {p.weekNum} {p.name} scored {p.score}{" "}
+            {p.started && `(${p.started})`}
           </div>
         ))}
     </div>
