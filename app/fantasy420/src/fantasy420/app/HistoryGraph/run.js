3cf2df94a59312aeaf282b2e22b63e0f295f2ff2
dcep93 <dcep93@gmail.com>
2024-08-30 04:11:58 -0400

    x

--- app/fantasy420/src/fantasy420/app/HistoryGraph/run.js
+++ app/fantasy420/src/fantasy420/app/HistoryGraph/run.js
@@ -1,43 +1,70 @@
+const numTickets = 10;
+const tickets = Array.from(new Array(numTickets)).map((_) => null);
+const queue = [];
+function takeTicket() {
+  const ticket = tickets.pop();
+  if (ticket === undefined) {
+    return new Promise((resolve) => queue.push(resolve));
+  }
+}
+
+function releaseTicket(t) {
+  tickets.push(null);
+  const resolve = queue.pop();
+  if (resolve) resolve();
+  return t;
+}
+
 function findALeague(start, size) {
+  var foundLeagueId = null;
+  var year = 2024;
   return Promise.resolve()
     .then(() =>
       Array.from(new Array(size))
         .map((_, i) => start + i)
         .map((leagueId) =>
-          fetch(
-            `https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/2023/segments/0/leagues/${leagueId}?view=kona_playercard`,
-            {
-              headers: {
-                accept: "application/json",
-                "x-fantasy-filter": JSON.stringify({
-                  players: {
-                    filterSlotIds: {
-                      value: [16],
-                    },
-                    filterStatsForTopScoringPeriodIds: {
-                      value: 17,
-                    },
+          Promise.resolve()
+            .then(takeTicket)
+            .then(() =>
+              fetch(
+                `https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/${
+                  year - 1
+                }/segments/0/leagues/${leagueId}?view=kona_playercard`,
+                {
+                  headers: {
+                    accept: "application/json",
+                    "x-fantasy-filter": JSON.stringify({
+                      players: {
+                        filterSlotIds: {
+                          value: [16],
+                        },
+                        filterStatsForTopScoringPeriodIds: {
+                          value: 17,
+                        },
+                      },
+                    }),
+                    "x-fantasy-platform":
+                      "kona-PROD-5b4759b3e340d25d9e1ae248daac086ea7c37db7",
+                    "x-fantasy-source": "kona",
                   },
-                }),
-                "x-fantasy-platform":
-                  "kona-PROD-5b4759b3e340d25d9e1ae248daac086ea7c37db7",
-                "x-fantasy-source": "kona",
-              },
-            }
-          )
-            .then((resp) => resp.json())
-            .then((resp) =>
-              resp.messages !== undefined
-                ? false
-                : Promise.resolve().then(() => true)
+                }
+              )
             )
+            .then((resp) => resp.json())
+            .then(releaseTicket)
+            .then((resp) => {
+              if (resp.messages === undefined) {
+                foundLeagueId = leagueId;
+                year = year - 1;
+              }
+            })
             .catch((e) => {
               console.log(e);
-              return null;
             })
         )
     )
     .then((ps) => Promise.all(ps))
+    .then(() => ({ foundLeagueId, year }))
     .then(JSON.stringify)
     .then(console.log);
 }
