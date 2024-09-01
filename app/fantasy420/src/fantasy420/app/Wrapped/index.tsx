58137ec46b5c96b5abe59192626d30e16810a59f
dcep93 <dcep93@gmail.com>
2024-08-31 20:56:24 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
@@ -43,6 +43,7 @@ export default function Wrapped() {
       Stacks,
       Negatives,
       UniquesStarted,
+      UniquesRostered,
       BoomBust,
       OwnedTeams,
       Benchwarmers,
@@ -1036,6 +1037,53 @@ function UniquesStarted() {
                         Object.entries(ffTeam.rosters).flatMap(
                           ([weekNum, roster]) =>
                             weekNum === "0" ? [] : roster.starting
+                        )
+                      )
+                    )
+                      .map(([playerId, c]) => ({
+                        player: selectedWrapped().nflPlayers[playerId],
+                        c,
+                      }))
+                      .filter(({ player }) => player?.position === Position[p])
+                      .map(({ player, c }) => `${player.name}: ${c}`),
+                  }))
+                  .sort((a, b) => a.started.length - b.started.length)
+                  .map(({ teamName, started }, i) => (
+                    <div key={i}>
+                      <b>
+                        {teamName}: ({started.length})
+                      </b>{" "}
+                      - {started.join(" , ")}
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+function UniquesRostered() {
+  return (
+    <div>
+      {Object.values(Position)
+        .filter((p) => Number.isInteger(p))
+        .map((p) => p as Position)
+        .filter((p) => p > 0)
+        .map((p) => (
+          <div key={p}>
+            <div style={bubbleStyle}>
+              {Position[p]}
+              <div>
+                {Object.values(selectedWrapped().ffTeams)
+                  .map((ffTeam) => ({
+                    teamName: ffTeam.name,
+                    started: Object.entries(
+                      Helpers.countStrings(
+                        Object.entries(ffTeam.rosters).flatMap(
+                          ([weekNum, roster]) =>
+                            weekNum === "0" ? [] : roster.rostered
                         )
                       )
                     )
