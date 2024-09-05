46e530108b42dd29caa62b228fbeefa19cd4f4cb
dcep93 <dcep93@gmail.com>
2024-09-05 01:05:49 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
@@ -42,7 +42,7 @@ export default function Wrapped() {
       ExtremeStuds,
       DraftValue,
       ByeSchedule,
-      NinthWonSecondLost,
+      SecondLost,
       json,
     }).map(([k, v]) => {
       try {
@@ -969,10 +969,63 @@ function ExtremeStuds() {
   );
 }
 
-function NinthWonSecondLost() {
+function SecondLost() {
   return (
     <div>
       <div>
+        <div>
+          <div style={bubbleStyle}>SecondLost</div>
+          <div>
+            {Object.entries(selectedWrapped().ffMatchups)
+              .map(([weekNum, matchups]) => ({
+                weekNum,
+                matchups,
+                scores: Object.values(selectedWrapped().ffTeams)
+                  .map((t) => ({
+                    t,
+                    score: t.rosters[weekNum].starting
+                      .map(
+                        (playerId) =>
+                          selectedWrapped().nflPlayers[playerId].scores[
+                            weekNum
+                          ]!
+                      )
+                      .reduce((a, b) => a + b, 0),
+                  }))
+                  .sort((a, b) => b.score - a.score),
+              }))
+              .map((o) => ({ ...o, second: o.scores[1].t.id }))
+              .map((o) => ({
+                ...o,
+                first: o.matchups
+                  .find((ms) => ms.includes(o.second))!
+                  .find((m) => m !== o.second)!,
+              }))
+              .map((o) => (
+                <div key={o.weekNum} style={bubbleStyle}>
+                  {o.scores.find((s) => s.t.id === o.second)!.score >
+                  o.scores.find((s) => s.t.id === o.first)!.score ? null : (
+                    <div>
+                      <div>week {o.weekNum}</div>
+                      <div>
+                        {selectedWrapped().ffTeams[o.second].name} lost to{" "}
+                        {selectedWrapped().ffTeams[o.first].name}
+                      </div>
+                      <div>
+                        {o.scores
+                          .find((s) => s.t.id === o.second)!
+                          .score.toFixed(2)}{" "}
+                        to{" "}
+                        {o.scores
+                          .find((s) => s.t.id === o.first)!
+                          .score.toFixed(2)}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+          </div>
+        </div>
         <div style={bubbleStyle}>NinthWon</div>
         <div>
           {Object.entries(selectedWrapped().ffMatchups)
@@ -1015,57 +1068,6 @@ function NinthWonSecondLost() {
                       to{" "}
                       {o.scores
                         .find((s) => s.t.id === o.tenth)!
-                        .score.toFixed(2)}
-                    </div>
-                  </div>
-                )}
-              </div>
-            ))}
-        </div>
-      </div>
-      <div>
-        <div style={bubbleStyle}>SecondLost</div>
-        <div>
-          {Object.entries(selectedWrapped().ffMatchups)
-            .map(([weekNum, matchups]) => ({
-              weekNum,
-              matchups,
-              scores: Object.values(selectedWrapped().ffTeams)
-                .map((t) => ({
-                  t,
-                  score: t.rosters[weekNum].starting
-                    .map(
-                      (playerId) =>
-                        selectedWrapped().nflPlayers[playerId].scores[weekNum]!
-                    )
-                    .reduce((a, b) => a + b, 0),
-                }))
-                .sort((a, b) => b.score - a.score),
-            }))
-            .map((o) => ({ ...o, second: o.scores[1].t.id }))
-            .map((o) => ({
-              ...o,
-              first: o.matchups
-                .find((ms) => ms.includes(o.second))!
-                .find((m) => m !== o.second)!,
-            }))
-            .map((o) => (
-              <div key={o.weekNum} style={bubbleStyle}>
-                {o.scores.find((s) => s.t.id === o.second)!.score >
-                o.scores.find((s) => s.t.id === o.first)!.score ? null : (
-                  <div>
-                    <div>week {o.weekNum}</div>
-                    <div>
-                      {selectedWrapped().ffTeams[o.second].name} lost to{" "}
-                      {selectedWrapped().ffTeams[o.first].name}
-                    </div>
-                    <div>
-                      {o.scores
-                        .find((s) => s.t.id === o.second)!
-                        .score.toFixed(2)}{" "}
-                      to{" "}
-                      {o.scores
-                        .find((s) => s.t.id === o.first)!
                         .score.toFixed(2)}
                     </div>
                   </div>
