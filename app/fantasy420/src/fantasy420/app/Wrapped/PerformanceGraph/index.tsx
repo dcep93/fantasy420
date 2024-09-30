eee324d9e00bad6ed29be4dd3f8dd92afbdcab5b
dcep93 <dcep93@gmail.com>
2024-09-30 05:06:21 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/PerformanceGraph/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/PerformanceGraph/index.tsx
@@ -62,8 +62,9 @@ export default function PerformanceGraph() {
     }))
     .map(({ weekNum, average }) => ({
       weekNum,
-      ...mapDict(selectedWrapped().ffTeams, (t) =>
-        Helpers.toFixed(raw[t.id].points[weekNum] - average)
+      ...mapDict(
+        selectedWrapped().ffTeams,
+        (t) => raw[t.id].points[weekNum] - average
       ),
     }));
   return (
@@ -71,7 +72,34 @@ export default function PerformanceGraph() {
       <ResponsiveContainer width="100%" height="100%">
         <LineChart data={data}>
           <XAxis dataKey="weekNum" />
-          <Tooltip />
+          <Tooltip
+            content={({ label, payload }) => (
+              <div
+                style={{
+                  background: "white",
+                  border: "1px solid black",
+                  padding: "1em",
+                  opacity: 0.8,
+                }}
+              >
+                <div>week {label}</div>
+                <div>
+                  {payload!
+                    .map(({ name, value, dataKey }) => ({
+                      name,
+                      dataKey,
+                      value: value as number,
+                    }))
+                    .sort((a, b) => b.value - a.value)
+                    .map((p) => (
+                      <div key={p.dataKey}>
+                        {Helpers.toFixed(p.value as number)}: {p.name}
+                      </div>
+                    ))}
+                </div>
+              </div>
+            )}
+          />
           {Object.values(selectedWrapped().ffTeams).map((t, index) => (
             <Line
               key={t.id}
