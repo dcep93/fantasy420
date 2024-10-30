f2b4cb8bf9dc9053509bda5ce4ad64a5ac7c2421
dcep93 <dcep93@gmail.com>
2024-10-30 13:40:23 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/index.tsx
@@ -45,6 +45,7 @@ export default function Wrapped() {
       Injuries,
       BestByPosition,
       ExtremeStuds,
+      AllTimeRecords,
       ByeSchedule,
       SecondLost,
       json,
@@ -1944,6 +1945,118 @@ export class Helpers {
       });
     return ideal;
   }
+}
+
+function AllTimeRecords() {
+  return (
+    <div>
+      {Object.entries({
+        manager_score: (year, wrapped) =>
+          Object.values(wrapped.ffTeams).flatMap((t) =>
+            Object.values(t.rosters)
+              .map(({ weekNum, starting }) => ({
+                year,
+                weekNum,
+                name: t.name,
+                value: starting
+                  .map(
+                    (playerId) =>
+                      wrapped.nflPlayers[playerId].scores[weekNum] || 0
+                  )
+                  .reduce((a, b) => a + b, 0),
+              }))
+              .filter(({ value }) => value !== 0)
+          ),
+        started_player_season_score: (year, wrapped) =>
+          Object.values(wrapped.nflPlayers)
+            .filter((p) =>
+              Object.values(wrapped.ffTeams)
+                .flatMap((t) =>
+                  Object.values(t.rosters).flatMap((r) => r.starting)
+                )
+                .includes(p.id)
+            )
+            .map((p) => ({
+              year,
+              weekNum: "0",
+              name: p.name,
+              value: p.scores["0"],
+            })),
+        player_week_score: (year, wrapped) =>
+          Object.values(wrapped.nflPlayers)
+            .filter((p) => p.position !== "DST")
+            .flatMap((p) =>
+              Object.entries(p.scores)
+                .map(([weekNum, score]) => ({
+                  year,
+                  weekNum,
+                  name: p.name,
+                  value: score,
+                }))
+                .filter(({ weekNum }) => weekNum !== "0")
+            ),
+        dst_week_score: (year, wrapped) =>
+          Object.values(wrapped.nflPlayers)
+            .filter((p) => p.position === "DST")
+            .flatMap((p) =>
+              Object.entries(p.scores)
+                .map(([weekNum, score]) => ({
+                  year,
+                  weekNum,
+                  name: p.name,
+                  value: score,
+                }))
+                .filter(({ weekNum }) => weekNum !== "0")
+            ),
+      } as {
+        [recordName: string]: (
+          year: string,
+          wrapped: WrappedType
+        ) => { year: string; weekNum: string; name: string; value: number }[];
+      })
+        .map(([recordName, f]) => ({
+          recordName,
+          sorted: Object.entries(allWrapped)
+            .flatMap(([year, yearWrapped]) => f(year, yearWrapped))
+            .sort((a, b) => b.value - a.value)
+            .map((o, i) => ({ ...o, i })),
+        }))
+        .map((o) => ({
+          ...o,
+          tops: o.sorted.slice(0, 20),
+          bottoms: o.sorted.slice(-20),
+        }))
+        .map(({ recordName, tops, bottoms }) => (
+          <div key={recordName} style={bubbleStyle}>
+            <h1>{recordName}</h1>
+            <table>
+              <tbody>
+                {[tops, [null], bottoms]
+                  .flatMap((s) => s)
+                  .map((o, i) => (
+                    <tr key={i}>
+                      {o === null ? (
+                        <td>----</td>
+                      ) : (
+                        <>
+                          <td>#{o.i + 1}</td>
+                          <td style={{ padding: "0 30px" }}>
+                            {o.year} w{o.weekNum}
+                          </td>
+                          <td style={{ padding: "0 30px" }}>
+                            {o.value.toFixed(2)}
+                          </td>
+                          <td>{o.name}</td>
+                        </>
+                      )}
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 function PlayerPlot() {
