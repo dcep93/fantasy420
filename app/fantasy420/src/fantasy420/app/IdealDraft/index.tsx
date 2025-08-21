86018e6ae700741679c98e180c0ea0c750f1360d
Daniel Cepeda <dcep93@gmail.com>
2025-08-21 00:30:36 -0400

    x

--- app/fantasy420/src/fantasy420/app/IdealDraft/index.tsx
+++ app/fantasy420/src/fantasy420/app/IdealDraft/index.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+import { selectedWrapped } from "../Wrapped";
+
 export default function IdealDraft() {
-  return <div>IdealDraft</div>;
+  const wrapped = selectedWrapped();
+  const [idealDraft, updateIdealDraft] = useState({
+    numAnalyzed: 0,
+    draft: Object.values(wrapped.ffTeams)
+      .flatMap((t) => t.draft)
+      .map(({ playerId, pickIndex: value }) => ({
+        playerId,
+        value,
+        points: wrapped.nflPlayers[playerId].total,
+        name: wrapped.nflPlayers[playerId].name,
+        analyzed: false,
+      }))
+      .sort((a, b) => a.value - b.value),
+  });
+  useEffect(() => {}, [idealDraft]);
+  const x = idealDraft;
+  return <pre>{JSON.stringify(x, null, 2)}</pre>;
 }
