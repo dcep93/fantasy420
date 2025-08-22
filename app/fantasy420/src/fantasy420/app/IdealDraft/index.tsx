350cc6a47cf3c8e4781c3721e4c592a45609d6f6
Daniel Cepeda <dcep93@gmail.com>
2025-08-22 18:38:21 -0400

    x

--- app/fantasy420/src/fantasy420/app/IdealDraft/index.tsx
+++ app/fantasy420/src/fantasy420/app/IdealDraft/index.tsx
@@ -49,53 +49,55 @@ function SubIdealDraft(props: { yearKey: string }) {
   return (
     <div>
       <div style={{ display: "flex", alignItems: "baseline" }}>
-        {drafts.map((d, i) => (
-          <div key={i} style={{ ...bubbleStyle, flexShrink: 0 }}>
-            <h1>
-              generation {i} ({d.draft.length})
-            </h1>
-            <div>
-              {Object.entries(groupByF(d.draft, (p) => p.ffTeamId))
-                .map(([ffTeamId, ps]) => ({
-                  ffTeamId,
-                  score: scoreTeam(ps),
-                  ps,
-                }))
-                .sort((a, b) => b.score - a.score)
-                .map(({ ffTeamId, score, ps }) => (
-                  <div
-                    key={ffTeamId}
-                    title={ps
-                      .map((p) => wrapped.nflPlayers[p.playerId].name)
-                      .join("\n")}
-                  >
-                    {ffTeamId}={score.toFixed(2)}
-                  </div>
-                ))}
-            </div>
-            <pre>
-              {d.draft
-                .map((p) => ({
-                  p,
-                  wp: wrapped.nflPlayers[p.playerId],
-                }))
-                .map((o, j) => (
-                  <div key={j}>
-                    <div style={bubbleStyle}>
-                      <div>
-                        #{j + 1} {o.wp.name}
-                      </div>
-                      <div>
-                        {o.wp.total} {o.wp.position}
-                        {playerIdToPositionSeasonRank[o.p.playerId] + 1}
-                      </div>
-                      <div>team {o.p.ffTeamId}</div>
+        {drafts
+          .map((d) => d.draft)
+          .map((d, i) => (
+            <div key={i} style={{ ...bubbleStyle, flexShrink: 0 }}>
+              <h1>
+                generation {i} ({d.length})
+              </h1>
+              <div>
+                {Object.entries(groupByF(d, (p) => p.ffTeamId))
+                  .map(([ffTeamId, ps]) => ({
+                    ffTeamId,
+                    score: scoreTeam(ps),
+                    ps,
+                  }))
+                  .sort((a, b) => b.score - a.score)
+                  .map(({ ffTeamId, score, ps }) => (
+                    <div
+                      key={ffTeamId}
+                      title={ps
+                        .map((p) => wrapped.nflPlayers[p.playerId].name)
+                        .join("\n")}
+                    >
+                      {ffTeamId}={score.toFixed(2)}
                     </div>
-                  </div>
-                ))}
-            </pre>
-          </div>
-        ))}
+                  ))}
+              </div>
+              <pre>
+                {d
+                  .map((p) => ({
+                    p,
+                    wp: wrapped.nflPlayers[p.playerId],
+                  }))
+                  .map((o, j) => (
+                    <div key={j}>
+                      <div style={bubbleStyle}>
+                        <div>
+                          #{j + 1} {o.wp.name}
+                        </div>
+                        <div>
+                          {o.wp.total} {o.wp.position}
+                          {playerIdToPositionSeasonRank[o.p.playerId] + 1}
+                        </div>
+                        <div>team {o.p.ffTeamId}</div>
+                      </div>
+                    </div>
+                  ))}
+              </pre>
+            </div>
+          ))}
       </div>
     </div>
   );
