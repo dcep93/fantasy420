33471ecfca6c4aab1f4aabc91702c45d3b430962
Daniel Cepeda <dcep93@gmail.com>
2025-08-23 00:46:36 -0400

    x

--- app/fantasy420/src/fantasy420/app/BuildIdealDraft/exec.ts
+++ app/fantasy420/src/fantasy420/app/BuildIdealDraft/exec.ts
@@ -1,6 +1,41 @@
 // npx ts-node ./exec.ts
 
 import allWrapped from "../Wrapped/allWrapped";
-import { generate } from "./search";
+import {
+  DraftPlayerType,
+  DraftType,
+  generate,
+  getPositionToRankedDraftPlayers,
+  getStart,
+  RosterEnum,
+} from "./search";
+
+type ConfigType = { year: string; rosterEnum: RosterEnum };
+
+function processCombination(
+  config: ConfigType
+): Promise<{ config: ConfigType; draftPlayers: DraftPlayerType[][] }> {
+  function helper(drafts: DraftType[]): Promise<{
+    config: ConfigType;
+    draftPlayers: DraftPlayerType[][];
+  }> {
+    return Promise.resolve()
+      .then(() =>
+        generate(drafts, positionToRankedDraftPlayers, config.rosterEnum)
+      )
+      .then((nextDrafts) =>
+        nextDrafts === null
+          ? { config, draftPlayers: drafts.map((d) => d.draft) }
+          : helper(nextDrafts)
+      );
+  }
+  const wrapped = allWrapped[config.year];
+  const positionToRankedDraftPlayers = getPositionToRankedDraftPlayers(wrapped);
+  return Promise.resolve()
+    .then(() =>
+      getStart(wrapped, positionToRankedDraftPlayers, config.rosterEnum)
+    )
+    .then(helper);
+}
 
 console.log(generate, Object.keys(allWrapped));
