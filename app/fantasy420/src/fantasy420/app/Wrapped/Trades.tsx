5edb3269b24b2080d7299fadd262de3bba525ef9
Daniel Cepeda <dcep93@gmail.com>
2025-09-01 12:44:47 -0400

    x

---
+++ app/fantasy420/src/fantasy420/app/Wrapped/Trades.tsx
@@ -0,0 +1,65 @@
+import { bubbleStyle, groupByF, selectedWrapped } from ".";
+
+export default function Trades() {
+  const playerIdToWeekNumToOwnerId: Record<string, Record<string, string>> = {};
+  Object.values(selectedWrapped().ffTeams).forEach((t) =>
+    Object.values(t.rosters).map((r) =>
+      r.rostered.forEach((playerId) => {
+        if (!playerIdToWeekNumToOwnerId[playerId])
+          playerIdToWeekNumToOwnerId[playerId] = {};
+        playerIdToWeekNumToOwnerId[playerId][r.weekNum] = t.id;
+      })
+    )
+  );
+  return (
+    <div>
+      {Object.entries(
+        groupByF(
+          Object.values(selectedWrapped().ffTeams)
+            .flatMap((t) =>
+              Object.values(t.rosters)
+                .filter((r) => r.weekNum !== "0" && r.weekNum !== "1")
+                .flatMap((r) =>
+                  r.rostered.map((playerId) => ({
+                    t,
+                    r,
+                    playerId,
+                    lastWeekOwnerId:
+                      playerIdToWeekNumToOwnerId[playerId]?.[
+                        Number(r.weekNum) - 1
+                      ],
+                  }))
+                )
+            )
+            .filter((o) => o.lastWeekOwnerId !== o.t.id && o.lastWeekOwnerId),
+          (o) => o.r.weekNum
+        )
+      ).map(([weekNum, os]) => (
+        <div key={weekNum}>
+          <div style={bubbleStyle}>
+            <div>week {weekNum}</div>
+            {os.map((o, i) => (
+              <div
+                key={i}
+                style={bubbleStyle}
+                title={Object.entries(
+                  selectedWrapped().nflPlayers[o.playerId].scores
+                )
+                  .flatMap(([scoreWeekNum, score]) =>
+                    scoreWeekNum === weekNum ? ["TRADE", score] : [score]
+                  )
+                  .join("\n")}
+              >
+                <div style={{ fontWeight: "bold" }}>
+                  {selectedWrapped().nflPlayers[o.playerId].name}
+                </div>
+                <div>{selectedWrapped().ffTeams[o.lastWeekOwnerId].name}</div>
+                <div>{selectedWrapped().ffTeams[o.t.id].name}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
