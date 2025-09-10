4aadd7d2d29051e38fd93381a428a834861e3a17
Daniel Cepeda <dcep93@gmail.com>
2025-09-09 20:32:07 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/tabs/PositionTrends.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/tabs/PositionTrends.tsx
@@ -1,3 +1,50 @@
+import { groupByF } from "..";
+import { playerStatsData } from "./PlayerStats";
+
 export default function PositionTrends() {
-  return <div>PositionTrends</div>;
+  return (
+    <pre>
+      {JSON.stringify(
+        Object.entries(
+          groupByF(
+            playerStatsData.flatMap((p) => p.years.map((y) => ({ p, y }))),
+            (o) => o.y.year.toString()
+          )
+        )
+          .map(([year, yearPlayers]) => ({
+            year: parseInt(year),
+            positions: Object.entries(
+              groupByF(yearPlayers, (p) => p.p.position)
+            ).map(([position, positionPlayers]) => ({
+              position,
+              x: Object.entries(
+                groupByF(
+                  positionPlayers
+                    .map(
+                      (p) => p.p.years.find((y) => y.year === p.y.year)!.scores
+                    )
+                    .flatMap((scores) =>
+                      scores.map((s, wMinusOne) => ({ s, w: wMinusOne + 1 }))
+                    ),
+                  (y) => y.w.toString()
+                )
+              )
+                .map(([w, scores]) => ({
+                  w,
+                  scores: scores.map(({ s }) => s!).filter(Boolean),
+                }))
+                .map((o) => ({
+                  w: parseInt(o.w),
+                  avg: o.scores.reduce((a, b) => a + b, 0) / o.scores.length,
+                }))
+                .sort((a, b) => a.w - b.w),
+            })),
+          }))
+          .sort((a, b) => b.year - a.year)
+          .filter(({ year }) => year !== 2025),
+        null,
+        2
+      ).slice(0, 10000)}
+    </pre>
+  );
 }
