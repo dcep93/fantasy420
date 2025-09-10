7b004faad4e40ce4a75d7834679c7dafdf066ca4
Daniel Cepeda <dcep93@gmail.com>
2025-09-09 21:10:19 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/tabs/PositionTrends.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/tabs/PositionTrends.tsx
@@ -6,7 +6,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import { bubbleStyle, clog, groupByF } from "..";
+import { bubbleStyle, groupByF } from "..";
 import { POSITION_COLORS } from "../../Draft";
 import { playerStatsData } from "./PlayerStats";
 
@@ -37,113 +37,35 @@ export default function PositionTrends() {
           )
             .map(([w, os]) => ({
               x: parseInt(w),
-              ys: Object.entries(groupByF(os, (o) => o.position)).map(
-                ([position, oos]) => ({
-                  position,
-                  avg: (({ scores }) =>
-                    scores.reduce((a, b) => a + b, 0) / scores.length)({
-                    scores: oos.map((ooo) => ooo.s!).filter(Boolean),
-                  }),
-                })
+              ys: Object.fromEntries(
+                Object.entries(groupByF(os, (o) => o.position)).map(
+                  ([position, oos]) => [
+                    position,
+                    (({ scores }) =>
+                      scores.reduce((a, b) => a + b, 0) / scores.length)({
+                      scores: oos.map((ooo) => ooo.s!).filter(Boolean),
+                    }),
+                  ]
+                )
               ),
             }))
             .sort((a, b) => a.x - b.x),
         }))
         .sort((a, b) => b.year - a.year)
-        .filter(({ year }) => year === 2024)
-        .map(clog)
         .map((o) => (
           <div key={o.year}>
             <div style={bubbleStyle}>
               <h1>{o.year}</h1>
-              <div>
-                <ResponsiveContainer width="80%" height="100%">
-                  <LineChart data={o.weeks}>
+              <div style={{ width: "500px", height: "500px" }}>
+                <ResponsiveContainer width="80%" height="80%">
+                  <LineChart
+                    data={o.weeks.map((oo) => ({ x: oo.x, ...oo.ys }))}
+                  >
                     <XAxis dataKey="x" />
                     <YAxis />
-                    <Tooltip
-                    //   content={({ label, payload, coordinate, viewBox }) => {
-                    //     if (
-                    //       domainData.year !== selectedYear ||
-                    //       label === undefined ||
-                    //       payload!.length === 0
-                    //     )
-                    //       return null;
-                    //     const mappedPayload = payload!
-                    //       .map(({ name, value, dataKey }) => ({
-                    //         name: name!,
-                    //         dataKey: dataKey!,
-                    //         value: value as number,
-                    //       }))
-                    //       .sort((a, b) => b.value - a.value);
-                    //     const cursorValue =
-                    //       domainData.min +
-                    //       domainData.range *
-                    //         (1 -
-                    //           (coordinate!.y! - viewBox!.y!) /
-                    //             viewBox!.height!);
-                    //     const closestTeamId = mappedPayload
-                    //       .map(({ dataKey, value }) => ({
-                    //         dataKey,
-                    //         value: Math.abs(value - cursorValue),
-                    //       }))
-                    //       .sort((a, b) => a.value - b.value)[0]
-                    //       .dataKey as string;
-                    //     setTimeout(() => updateSelectedTeamId(closestTeamId));
-                    //     const values = data.find((d) => d.x === label)!.ys.data;
-                    //     return (
-                    //       <div
-                    //         style={{
-                    //           background: "white",
-                    //           border: "1px solid black",
-                    //           padding: "1em",
-                    //           opacity: 0.8,
-                    //         }}
-                    //       >
-                    //         <div style={{ textDecoration: "underline" }}>
-                    //           week{" "}
-                    //           {key === "fantasyCalc"
-                    //             ? Helpers.toFixed(
-                    //                 (label -
-                    //                   selectedWrapped().fantasyCalc.history[0]
-                    //                     .date) /
-                    //                   (1000 * 60 * 60 * 24 * 7),
-                    //                 4
-                    //               )
-                    //             : label}
-                    //         </div>
-                    //         <div>
-                    //           {mappedPayload.map((p) => (
-                    //             <div
-                    //               key={p.dataKey}
-                    //               style={{
-                    //                 fontWeight:
-                    //                   p.dataKey === closestTeamId
-                    //                     ? "bold"
-                    //                     : undefined,
-                    //               }}
-                    //             >
-                    //               {key === "fantasyCalc" ? (
-                    //                 values[p.dataKey].toFixed(2)
-                    //               ) : (
-                    //                 <>
-                    //                   {values[p.dataKey].toFixed(2)}: (
-                    //                   {dataB[p.dataKey].wins[label]})
-                    //                 </>
-                    //               )}{" "}
-                    //               {selectedWrapped().ffTeams[p.name].name}
-                    //             </div>
-                    //           ))}
-                    //         </div>
-                    //       </div>
-                    //     );
-                    //   }}
-                    />
-                    {Object.keys(
-                      groupByF(
-                        o.weeks.flatMap((oo) => oo.ys),
-                        (oo) => oo.position
-                      )
+                    <Tooltip />
+                    {Array.from(
+                      new Set(o.weeks.flatMap((w) => Object.keys(w.ys)))
                     )
                       .map((position) => ({
                         position,
