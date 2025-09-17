b79b5f3a6ef100bd3f7912fb8dddb36b1686dff2
Daniel Cepeda <dcep93@gmail.com>
2025-09-16 22:21:35 -0400

    x

--- app/fantasy420/src/fantasy420/app/Wrapped/tabs/PlayerStats/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/tabs/PlayerStats/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { bubbleStyle } from "../..";
+import allWrapped from "../../allWrapped";
 import rawData from "./data.json";
 
 export const playerStatsData = rawData as {
@@ -13,29 +14,68 @@ const MAX_RESULTS = 100;
 
 // https://nflquery.web.app/fantasy
 export default function PlayerStats() {
-  const [nameFilter, updateNameFilter] = useState("");
+  const [nameFilter, updateNameFilter] = useState("aj brown");
   return (
     <div>
       <div>
         nameFilter:{" "}
         <input
           value={nameFilter}
-          onChange={(e) =>
-            updateNameFilter(e.currentTarget.value.toLowerCase())
-          }
+          onChange={(e) => updateNameFilter(normalize(e.currentTarget.value))}
         />
       </div>
       <div style={{ display: "flex" }}>
         {playerStatsData
-          .filter((d) => d.name.toLowerCase().includes(nameFilter))
+          .filter((d) => normalize(d.name).includes(nameFilter))
           .sort((a, b) => b.total - a.total)
           .slice(0, MAX_RESULTS)
           .map((d, i) => (
             <div key={i} style={bubbleStyle}>
-              <pre>{JSON.stringify({ index: i + 1, ...d }, null, 2)}</pre>
+              <pre>
+                {JSON.stringify(
+                  {
+                    index: i + 1,
+                    ...d,
+                    years: d.years
+                      .slice()
+                      .reverse()
+                      .map((y) => ({ y, w: allWrapped[y.year] }))
+                      .map((o) => ({
+                        ...o,
+                        id: Object.values(o.w?.nflPlayers || {}).find(
+                          (p) => p.name === d.name
+                        )?.id,
+                      }))
+                      .map((o) => ({
+                        ...o,
+                        owner: Object.values(o.w?.ffTeams || {}).find((t) =>
+                          t.rosters[0].rostered.includes(o.id!)
+                        )?.name,
+                      }))
+                      .map((o) => ({
+                        owner: o.owner,
+                        ...o.y,
+                        scores: o.y.scores.map((score, i) =>
+                          (({ owner }) =>
+                            owner ? { week: i + 1, owner, score } : score)({
+                            owner: Object.values(o.w?.ffTeams || {}).find((t) =>
+                              t.rosters[i + 1]?.rostered.includes(o.id!)
+                            )?.name,
+                          })
+                        ),
+                      })),
+                  },
+                  null,
+                  2
+                )}
+              </pre>
             </div>
           ))}
       </div>
     </div>
   );
+}
+
+function normalize(s: string) {
+  return s.toLowerCase().replaceAll(".", "");
 }
