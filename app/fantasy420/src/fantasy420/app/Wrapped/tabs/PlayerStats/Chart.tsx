2900c24a7c26733547a4e92ee4219e937a07f81f
Daniel Cepeda <dcep93@gmail.com>
2025-10-31 22:08:29 -0400

    x

---
+++ app/fantasy420/src/fantasy420/app/Wrapped/tabs/PlayerStats/Chart.tsx
@@ -0,0 +1,10 @@
+export default function Chart(props: {
+  scores: {
+    week: number;
+    state: string;
+    score: number | null;
+    owner: string | undefined;
+  }[];
+}) {
+  return <div>chart</div>;
+}
--- app/fantasy420/src/fantasy420/app/Wrapped/tabs/PlayerStats/index.tsx
+++ app/fantasy420/src/fantasy420/app/Wrapped/tabs/PlayerStats/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { bubbleStyle, currentYear } from "../..";
+import { currentYear } from "../..";
 import allWrapped from "../../allWrapped";
+import Chart from "./Chart";
 import rawData from "./data.json";
 
 export const playerStatsData = rawData as {
@@ -24,74 +25,79 @@ export default function PlayerStats() {
           onChange={(e) => updateNameFilter(normalize(e.currentTarget.value))}
         />
       </div>
-      <div style={{ display: "flex" }}>
+      <div>
         {playerStatsData
           .filter((d) => normalize(d.name).includes(nameFilter))
           .sort((a, b) => b.total - a.total)
           .slice(0, MAX_RESULTS)
           .map((d, i) => (
-            <div key={i} style={bubbleStyle}>
+            <div key={i} style={{ display: "flex" }}>
               <pre>
                 {JSON.stringify(
-                  {
-                    index: i + 1,
-                    ...d,
-                    years: d.years
-                      .slice()
-                      .reverse()
-                      .map((y) => ({
-                        ...y,
-                        // total for year and career
-                        // will be stale
-                        scores:
-                          y.year.toString() !== currentYear
-                            ? y.scores
-                            : Object.values(
-                                Object.values(
-                                  allWrapped[y.year].nflPlayers
-                                ).find((p) => p.name === d.name)?.scores || []
-                              ).slice(1),
-                      }))
-                      .map((y) => ({ y, w: allWrapped[y.year] }))
-                      .map((o) => ({
-                        ...o,
-                        id: Object.values(o.w?.nflPlayers || {}).find(
-                          (p) => p.name === d.name
-                        )?.id,
-                      }))
-                      .map((o) => ({
-                        ...o,
-                        owner: Object.values(o.w?.ffTeams || {}).find((t) =>
-                          t.rosters[0].rostered.includes(o.id!)
-                        )?.name,
-                      }))
-                      .map((o) => ({
-                        owner: o.owner,
-                        ...o.y,
-                        scores: o.y?.scores.map((score, i) =>
-                          (({ owner }) =>
-                            owner
-                              ? {
-                                  week: i + 1,
-                                  [owner.rosters[i + 1]?.starting.includes(
-                                    o.id!
-                                  )
-                                    ? "started"
-                                    : "benched"]: owner.name,
-                                  score,
-                                }
-                              : score)({
-                            owner: Object.values(o.w?.ffTeams || {}).find((t) =>
-                              t.rosters[i + 1]?.rostered.includes(o.id!)
-                            ),
-                          })
-                        ),
-                      })),
-                  },
+                  { index: i + 1, ...d, years: undefined },
                   null,
                   2
                 )}
               </pre>
+              <div style={{ display: "flex" }}>
+                {d.years
+                  .slice()
+                  .reverse()
+                  .map((y) => ({
+                    ...y,
+                    // total for year and career
+                    // will be stale
+                    scores:
+                      y.year.toString() !== currentYear
+                        ? y.scores
+                        : Object.values(
+                            Object.values(allWrapped[y.year].nflPlayers).find(
+                              (p) => p.name === d.name
+                            )?.scores || []
+                          ).slice(1),
+                  }))
+                  .map((y) => ({ y, w: allWrapped[y.year] }))
+                  .map((o) => ({
+                    ...o,
+                    id: Object.values(o.w?.nflPlayers || {}).find(
+                      (p) => p.name === d.name
+                    )?.id,
+                  }))
+                  .map((o) => ({
+                    ...o,
+                    owner: Object.values(o.w?.ffTeams || {}).find((t) =>
+                      t.rosters[0].rostered.includes(o.id!)
+                    )?.name,
+                  }))
+                  .map((o) => ({
+                    owner: o.owner,
+                    ...o.y,
+                    scores: o.y?.scores.map((score, i) =>
+                      (({ owner }) => ({
+                        week: i + 1,
+                        state: !owner
+                          ? "unowned"
+                          : owner?.rosters[i + 1]?.starting.includes(o.id!)
+                          ? "started"
+                          : "benched",
+                        score,
+                        owner: owner?.name,
+                      }))({
+                        owner: Object.values(o.w?.ffTeams || {}).find((t) =>
+                          t.rosters[i + 1]?.rostered.includes(o.id!)
+                        ),
+                      })
+                    ),
+                  }))
+                  .map((o) => (
+                    <div>
+                      <pre>
+                        {JSON.stringify({ ...o, scores: undefined }, null, 2)}
+                      </pre>
+                      <Chart scores={o.scores} />
+                    </div>
+                  ))}
+              </div>
             </div>
           ))}
       </div>
