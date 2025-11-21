bbe68c3489b762852516a08b406e2c69ecc7c5d6
Daniel Cepeda <dcep93@gmail.com>
2025-11-21 01:53:34 -0500

    Convert app to Vite

--- app/fantasy420/src/setupTests.ts
+++ app/fantasy420/src/setupTests.ts
@@ -2,4 +2,26 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom/vitest";
+
+class ResizeObserver {
+  callback: ResizeObserverCallback;
+
+  constructor(callback: ResizeObserverCallback) {
+    this.callback = callback;
+  }
+
+  observe(target: Element) {
+    this.callback([], this);
+  }
+
+  unobserve() {
+    // noop
+  }
+
+  disconnect() {
+    // noop
+  }
+}
+
+global.ResizeObserver = ResizeObserver;
