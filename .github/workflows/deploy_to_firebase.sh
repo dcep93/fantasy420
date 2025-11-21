6cf00ea66448b0836bf8f11345ad769eff13b434
Daniel Cepeda <dcep93@gmail.com>
2025-11-21 12:16:38 -0500

    x

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -4,27 +4,31 @@ set -euo pipefail
 
 SA_KEY="$1"
 
-# # disable billing
-# # create firebase project
-# nvm install 16.4.0
-# firebase init hosting --project "$GOOGLE_CLOUD_PROJECT"
+# set -e
+# gcloud billing projects unlink $GOOGLE_CLOUD_PROJECT
+# gcloud services enable firebase.googleapis.com
+# firebase projects:addfirebase $GOOGLE_CLOUD_PROJECT
+# yes "" | firebase init hosting --project "$GOOGLE_CLOUD_PROJECT"
 # gcloud iam service-accounts create deployer-github
 # gcloud projects add-iam-policy-binding "$GOOGLE_CLOUD_PROJECT" --member="serviceAccount:deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" --role="roles/firebasehosting.admin"
 # gcloud iam service-accounts keys create gac.json --iam-account "deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com"
+# echo; echo; echo
 # cat gac.json
+# echo; echo; echo
 
 cd app/fantasy420
 
 export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
-echo "$SA_KEY" >"$GOOGLE_APPLICATION_CREDENTIALS"
+echo "$SA_KEY" > "$GOOGLE_APPLICATION_CREDENTIALS"
 npm install -g firebase-tools
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
 project_id="$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq -r .project_id)"
 
-cat <<EOF >firebase.json
+
+cat <<EOF > firebase.json
 {
     "hosting": {
-        "public": "build",
+        "public": "dist",
         "ignore": ["firebase.json", "**/.*", "**/node_modules/**"],
         "rewrites": [{
             "source": "**",
@@ -34,7 +38,7 @@ cat <<EOF >firebase.json
 }
 EOF
 
-cat <<EOF >.firebaserc
+cat <<EOF > .firebaserc
 {
     "projects": {
         "default": "$project_id"
